8fbe5d08a8d5047aa63e4e96681aadd97d55385e
priya <priyabratamulia@gmail.com>
2017-07-12 17:32:38 +0530

    Priya

---
+++ square.py
@@ -0,0 +1,45 @@
+#!usr/bin/python
+#jjjjjSquare of 1 to 10 numbers?
+
+list=range(1,11)
+print 'List =',list
+print ('\nSquare of the list variable')
+
+for x in list:
+	print x,'*',x,'=',(x**2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
